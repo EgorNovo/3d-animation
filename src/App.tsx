fa672824811346cc3dd813cfbd480ab4cd51c80e
Casle <egor.n.fe@yandex.ru>
2024-12-23 15:38:30 +0500

    feat: add 3d cube animation

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <div className="container">
+        <div className="back side"></div>
+        <div className="left side"></div>
+        <div className="right side"></div>
+        <div className="top side"></div>
+        <div className="bottom side"></div>
+        <div className="front side"></div>
+    </div>
+  )
+}
+
+export default App
